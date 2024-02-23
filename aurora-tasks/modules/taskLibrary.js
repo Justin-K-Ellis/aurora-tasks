fa8d6c7babe911746c157aed9436149de537aa64
Justin <jlke45@gmail.com>
2024-02-23 12:12:32 +0900

    Create module to make task library.

---
+++ aurora-tasks/modules/taskLibrary.js
@@ -0,0 +1,13 @@
+// Factory functiom to create an object ("library") to store tasks to 
+// be rendered to the DOM.
+
+function makeTaskLibrary() {
+    let taskArray = [];
+
+    // TODO: method to delete tasks
+    const deleteTask = () => {
+        console.log("todo");
+    }
+
+    return { taskArray, deleteTask }
+}
