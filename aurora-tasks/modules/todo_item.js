41cfcb6554499e91999c034f89a59e6403cbf647
Justin <jlke45@gmail.com>
2024-02-24 13:16:41 +0900

    Rename module to taskItem.js

--- aurora-tasks/modules/todo_item.js
+++
@@ -1,8 +0,0 @@
-// Factory function to make todo items
-
-const makeTodo = (title, description, date) => {
-    let done = false;
-    return { title, description, date, done};
-}
-
-export default makeTodo;
